48557a187329f1293131b9c6402014230feebd53
Fedor Staroverov <Fstaroverov@gmail.com>
2023-08-23 05:31:46 +0400

    Fix homework

--- blogicum/blog/utils.py
+++ blogicum/blog/utils.py
@@ -1,8 +1,8 @@
 def get_short_string(full_string: str, max_words: int = 5) -> str:
     """
     Takes in a string full_string and int optional argument max_words,
-    max_words defaults to 5.
-    returns a string with the number of words equal to max_words,
+    max_words defaults to 5,
+    returns a string with the number of words equal to max_words or less.
     """
 
     string_words = full_string.split()
